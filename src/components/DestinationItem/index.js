3d9e3137f6d1a47e6d83764953f41228fc716bdb
Subhasish08 <128680123+Subhasish08@users.noreply.github.com>
2023-06-16 18:35:27 +0530

    Update index.js

--- src/components/DestinationItem/index.js
+++ src/components/DestinationItem/index.js
@@ -3,11 +3,11 @@ import './index.css'
 
 const DestinationItem = props => {
   const {DestinationDetails} = props
-  const {name, imageUrl} = DestinationDetails
+  const {name, imgUrl} = DestinationDetails
 
   return (
     <li className="user-card-container">
-      <img src={imageUrl} className="avatar" alt={name} />
+      <img src={imgUrl} className="avatar" alt={name} />
       <p className="description">{name}</p>
     </li>
   )
